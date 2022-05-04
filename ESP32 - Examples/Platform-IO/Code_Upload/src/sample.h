10a8e8071473399e58878f8ac290d4dfc372c291
Adhiban Siddarth V <siddhu1131@gmail.com>
2022-05-04 12:13:37 +0530

    changed

--- ESP32 - Examples/Platform-IO/Code_Upload/src/sample.h
+++ ESP32 - Examples/Platform-IO/Code_Upload/src/sample.h
@@ -1,23 +1,15 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
-
-Servo downservo, upservo;
+// #include <ESP32Servo.h>
 
 void setup()
 {
-    downservo.attach(13);
-    upservo.attach(12);
     Serial.begin(9600);
 }
-int a = 2000;
+
 void loop()
 {
-    downservo.write(150);
-    delay(50);
-    upservo.write(180 - 60);
-    delay(a);
-    downservo.write(90);
-    delay(50);
-    upservo.write(180 - 180);
-    delay(a);
+    if (Serial.available())
+    {
+        Serial.println(Serial.read());
+    }
 }
